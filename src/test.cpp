82089241aaf0b84ad43f3362b2235b4e0df0575d
RedBrumbler <redbrumbler.official@gmail.com>
2021-08-04 19:33:40 +0200

    Add some actual code to test it actually compiling

--- src/test.cpp
+++ src/test.cpp
@@ -5,3 +5,14 @@
 #include "QosmeticsAPI.hpp"
 #include "SaberAPI.hpp"
 #include "WallAPI.hpp"
+
+using namespace Qosmetics;
+int main ()
+{
+    // this at least makes it so nothing gets stripped
+    TrailHelper* ptr = new TrailHelper();
+    ColorComponent* ptr2 = new ColorComponent();
+    AltTrail* ptr3 = new AltTrail();
+
+    return 0;
+}
